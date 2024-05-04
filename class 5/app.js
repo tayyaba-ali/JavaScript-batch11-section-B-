83beac41118a7686eab58aa298e78ec9bb6fcb8b
tayyaba-ali <ta6402792@gmail.com>
2024-05-04 11:17:26 +0500

    class 5 done

---
+++ class 5/app.js
@@ -0,0 +1,71 @@
+// alert("welcome");
+
+// console.log("marwa2"+"rida"+33);
+
+// var num = 17;
+// alert("You have visited "+num+"times")
+
+// document.write("<h1>Tayyaba &#176; </h1>")
+
+// var tableNum = 99;
+// var counter = 1;
+// var incre = 1;
+// document.write(
+//     tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" +
+//     tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" +
+//     tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" +
+//     tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" + tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" +
+//     tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" + tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" +
+//     tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" +
+//     tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" +
+//     tableNum + "X" + counter++ + "=" + tableNum * incre++ + "<br>" 
+
+
+// );
+
+// var num = +prompt("enter your number");
+// console.log(num+5);
+
+// if krne ke baad aengen
+    var num1 = +prompt("Enter your first number");
+    var operator = prompt("enter your operator")
+    var num2  = +prompt("Enter your second number");
+
+var result ;
+if(operator == "+"){
+   result=  num1+num2
+}
+if(operator == "-"){
+    result=  num1-num2
+}
+if(operator == "*"){
+    result=  num1*num2
+}
+if(operator == "/"){
+    result=  num1/num2
+}
+alert(result)
+
+
+
+
+
+
+
+// console.log("Tayyaba" !== "2");
+
+
+// if 
+
+// var usernum = 3
+
+// if(usernum==2){
+//  console.log("chl gya");
+// }
+// if(usernum == 3){
+//     console.log("3 number chlgya");
+// }
+// if(usernum == 3){
+//     console.log("3 ra chala ha");
+// }
+
